b700d2f8105259d409816a3cd25e20db74428579
pablopenna <p.pena@edu.uah.es>
2018-02-24 13:02:04 +0100

    Not really solved. Testing.

--- wsgi.py
+++ wsgi.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 def hello():
     return "Welcome to the main page!"
 
+"""
 @app.route("/<nombre>")
 def greet(nombre):
 	return "Hello there, "+nombre
-
+"""
 if __name__ == "__main__":
     app.run()
