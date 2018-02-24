eb7725450a948b69f12aaaaad46dd92d5db84186
pablopenna <p.pena@edu.uah.es>
2018-02-24 13:05:15 +0100

    More testing

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,14 @@
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def hello():
     return "Welcome to the main page!"
 
-"""
-@app.route("/<nombre>")
-def greet(nombre):
-	return "Hello there, "+nombre
-"""
+
+#@application.route("/<nombre>")
+#def greet(nombre):
+#	return "Hello there, "+nombre
+
 if __name__ == "__main__":
-    app.run()
+    application.run()
