461acc23303542fec47ab0594cab5ddeffa2756c
Pablo <p.pena@edu.uah.es>
2018-02-26 13:47:54 +0100

    Fixing errors.

--- wsgi.py
+++ wsgi.py
@@ -52,4 +52,4 @@ def logout():
 if __name__ == '__main__':
     app.config["SECRET_KEY"] = "ITSASECRET"
     #app.run(port=5000,debug=True)
-    app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
+    app.run(threaded=True)
